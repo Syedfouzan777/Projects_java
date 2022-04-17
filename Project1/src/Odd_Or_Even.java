40dc133ca2ee11590ba0dfbb29293afb6fccd7c1
fouzan u <fouzan u@DESKTOP-IOE0TJI>
2022-04-17 11:07:37 +0530

    First Commit

---
+++ Project1/src/Odd_Or_Even.java
@@ -0,0 +1,16 @@
+
+public class Odd_Or_Even {
+
+	public static void main(String[] args) {
+		int num = 12;
+		
+		if(num % 2 ==0) {
+			System.out.println("The Number Is Even");
+		}
+		else {
+			System.out.println("The Number is odd");
+		}
+
+	}
+
+}
---
+++ Project1/src/Quotient_Remainder.java
@@ -0,0 +1,9 @@
+public class Quotient_Remainder  {
+    public static void main(String[] args) {
+        int num1 = 17, num2 = 2;
+        int quotient = num1 / num2;
+        int remainder = num1 % num2;
+        System.out.println("Quotient is: " + quotient);
+        System.out.println("Remainder is: " + remainder);
+    }
+}
